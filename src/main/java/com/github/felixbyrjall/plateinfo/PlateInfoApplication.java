5178bf7358203c8245767d9882b5e03748492016
Felix Byrjall <felixbyrjall@gmail.com>
2024-10-02 19:00:40 +0200

    chore: initial commit set up with spring boot

---
+++ src/main/java/com/github/felixbyrjall/plateinfo/PlateInfoApplication.java
@@ -0,0 +1,13 @@
+package com.github.felixbyrjall.plateinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlateInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlateInfoApplication.class, args);
+	}
+
+}
